e9fea68c0e9db5a389e135bb6b4e7f191cfc7da7
vishthefish0001 <vishnups0001@gmail.com>
2026-04-30 15:35:42 -0400

    Update input.h, add get_continuation_jump dec

--- input.h
+++ input.h
@@ -6,4 +6,7 @@
 
 Move get_player_move(const GameState *state);
 
+//called during multi jump turns, piece is fixed, so the player chooses the end position
+Move get_continuation_jump(const GameState *state, int r, int c);
+
 #endif // INPUT_H
