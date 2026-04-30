32339f2537d30341930fe4dd78fa8e12140727d3
vishthefish0001 <vishnups0001@gmail.com>
2026-04-30 16:18:24 -0400

    Update input.c, added the jumping and multi jump

--- input.c
+++ input.c
@@ -1,69 +1,81 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "input.h" 
+#include "moves.h"
 
 static void flush_stdin(void) {
     int c;
     while ((c = getchar()) != '\n' && c != EOF);
 }
 
-Move get_player_move(const GameState *state) {
-    Move m;
-    int valid = 0;
-    const char *name = (state->current_player == RED) ? "Red" : "Black";
+static int read_destination(Move *m) {
+    char dest_col;
+    char dest_row;
 
-    int valid_pieces[64][2]; 
-    int num_pieces = 0;
+    printf("Enter Destination, ex: C4");
+    if (scanf(" %c%d" &dest_col, &dest_row) !=2) {
+        if(feof(stdin)) exit(1);
+        flush_stdin();
+        return 0;
+    }
+    flush_stdin();
 
-    // Scan the board to find which pieces are allowed to move
-    int must_jump = must_player_jump(state, state->current_player);
+    if (dest_col >= 'a' && dest_col <= 'z') //accepting lowercase inputs
+        dest_col = dest_col - 'a' + 'A';
 
+    m->to_col = dest_col - 'A';
+    m->to_row = 8 - dest_row;
+
+    if (m->to_col < 0 || m->to_col >= BOARD_SIZE ||
+        m->to_row < 0 || m->to_row >= BOARD_SIZE) {
+        printf("  Error: coordinates out of bounds.\n\n");
+        return 0;
+    }
+    return 1;
+}
+
+//This will build the menu of movable pieces for the crurrent player, enorcing jump rule
+//Will retun  the valid pieces, so when its 0 a winner is delcared
+static int build_piece_menu(const GameState *state, int valid_pieces[][2], int must_jump){
+    int num = 0;
     for (int r = 0; r < BOARD_SIZE; r++) {
         for (int c = 0; c < BOARD_SIZE; c++) {
-            if (state->board[r][c].color == state->current_player) {
-                if (must_jump) { 
-                    if (can_piece_jump(state, r, c)) {
-                        valid_pieces[num_pieces][0] = r;
-                        valid_pieces[num_pieces][1] = c;
-                        num_pieces++;
-                    }
-                } else { 
-                    if (can_piece_move(state, r, c)) {
-                        valid_pieces[num_pieces][0] = r;
-                        valid_pieces[num_pieces][1] = c;
-                        num_pieces++;
-                    }
-                }
+            if (state->board[r][c].color != state->current_player) continue;
+            int ok = must_jump ? can_piece_jump(state, r, c)
+                               : can_piece_move(state, r, c);
+            if (ok) {
+                valid_pieces[num][0] = r;
+                valid_pieces[num][1] = c;
+                num++;
             }
         }
     }
+    return num;
+}
 
-    if (num_pieces == 0) {
-        printf("%s has no legal moves! Game over.\n", name);
-        exit(0);
-    }
+//The player will pick a piece from menu and then its destination, 
+//loppong until a legal move is entered
+Move get_player_move(const GameState *state) {
+    Move m;
+    const char *name  = (state->current_player == RED) ? "Red" : "Black";
+    int must_jump = must_player_jump(state, state->current_player);
+    int valid_pieces[64][2];
+    int num_pieces = build_piece_menu(state, valid_pieces, must_jump);
 
-    // Display the menu and get input
-    while (!valid) {
-        printf("%s's turn. Available pieces to move:\n", name);
-        
+    while (1) {
+        printf("%s's turn. Available pieces:\n", name);
         for (int i = 0; i < num_pieces; i++) {
             int pr = valid_pieces[i][0];
             int pc = valid_pieces[i][1];
-            int is_king = state->board[pr][pc].is_king;
-
-            char col_letter = 'A' + pc;
-            int row_number = 8 - pr;
-
-            if (is_king) {
-                printf("  %d) King at %c%d\n", i + 1, col_letter, row_number);
-            } else {
-                printf("  %d) Piece at %c%d\n", i + 1, col_letter, row_number);
-            }
+            printf("  %d) %s at %c%d\n",
+                   i + 1,
+                   state->board[pr][pc].is_king ? "King" : "Piece",
+                   'A' + pc, 8 - pr);
         }
 
+        // Pick piece 
         int choice;
-        printf("Enter the number of the piece to select: ");
+        printf("Select piece number: ");
         if (scanf("%d", &choice) != 1 || choice < 1 || choice > num_pieces) {
             if (feof(stdin)) exit(1);
             printf("  Error: invalid choice.\n\n");
@@ -75,40 +87,53 @@ Move get_player_move(const GameState *state) {
         m.from_row = valid_pieces[choice - 1][0];
         m.from_col = valid_pieces[choice - 1][1];
 
-        // Ask for destination
-        char dest_col;
-        int dest_row;
-        printf("Enter destination (e.g., C4): ");
-        if (scanf(" %c%d", &dest_col, &dest_row) != 2) {
-            if (feof(stdin)) exit(1);
+        // Pick destination 
+        if (!read_destination(&m)) {
             printf("  Error: invalid format.\n\n");
-            flush_stdin();
-            continue;
-        }
-        flush_stdin();
-
-        // Manual case conversion
-        if (dest_col >= 'a' && dest_col <= 'z') {
-            dest_col = dest_col - 'a' + 'A'; 
-        }
-
-        m.to_col = dest_col - 'A';
-        m.to_row = 8 - dest_row;
-
-        if (m.to_col < 0 || m.to_col >= BOARD_SIZE || m.to_row < 0 || m.to_row >= BOARD_SIZE) {
-            printf("  Error: coordinates out of bounds.\n\n");
             continue;
         }
 
         Move full = get_full_move(state, m);
         if (full.jumped_row == -2) {
-            printf("  Error: illegal destination. Try again.\n\n");
+            printf("  Error: illegal destination.\n\n");
             continue;
         }
 
-        m = full;
-        valid = 1;
-    }
+        
+        // Enforce forced-jump: if a jump was available but the player
+        // entered a simple move, reject it.
+        if (must_jump && full.jumped_row == -1) {
+            printf("  Error: you must jump.\n\n");
+            continue;
+        }
 
-    return m;
+        return full;
+    }
+}
+//input is the piece fixed at (r,c)
+//used by multi jump in main.c, player pick the jump point
+Move get_continuation_jump(const GameState *state, int r, int c) {
+    Move m;
+    m.from_row = r;
+    m.from_col = c;
+
+    while (1) {
+        printf("Continue jumping with piece at %c%d.\n",
+               'A' + c, 8 - r);
+
+        if (!read_destination(&m)) {
+            printf("  Error: invalid format.\n\n");
+            continue;
+        }
+
+        Move full = get_full_move(state, m);
+
+        // Must be a jump (not a simple step) 
+        if (full.jumped_row < 0) {
+            printf("  Error: must continue jumping.\n\n");
+            continue;
+        }
+
+        return full;
+    }
 }
