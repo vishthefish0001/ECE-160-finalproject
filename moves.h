f92ca7236c480d8525b9473692093ee8d180e822
vishthefish0001 <vishnups0001@gmail.com>
2026-05-05 17:22:00 -0400

    adding more comments

--- moves.h
+++ moves.h
@@ -4,11 +4,11 @@
 #include "board.h"
 
 // functions — used by board.c (render) and input.c (menu) */
-int must_player_jump(const GameState *state, int player_color);
+int must_player_jump(const GameState *state, int player_color); //used for getting piece highlights and moves in terminal
 int can_piece_jump(const GameState *state, int r, int c);
 int can_piece_move(const GameState *state, int r, int c);
 
-// Validates destination and fills in jumped_row/jumped_col 
+// return a movs with jumped_row = -1 (simple), >=0 (jump), or -2 (illegal) move 
 Move get_full_move(const GameState *state, Move m);
 
 // Executes the move: moves piece, removes captured piece, crowns kings 
