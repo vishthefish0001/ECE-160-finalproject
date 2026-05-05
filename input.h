174efcb6c75bc20065a166e2f19dd286d0d5fb5f
vishthefish0001 <vishnups0001@gmail.com>
2026-05-05 17:42:03 -0400

    adding comments

--- input.h
+++ input.h
@@ -4,9 +4,10 @@
 #include "board.h"
 #include "moves.h"
 
+//full turns are defined as player picking legal move and typing destination
 Move get_player_move(const GameState *state);
 
-//called during multi jump turns, piece is fixed, so the player chooses the end position
+//called during multi jump turns, piece is fixed at (r,c), so the player chooses the end position
 Move get_continuation_jump(const GameState *state, int r, int c);
 
-#endif // INPUT_H
+#endif 
